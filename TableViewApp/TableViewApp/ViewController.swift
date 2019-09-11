19682cf946773e993a03e864c1b91bdc405ff941
Imtiyaz Ahmad <52881580+imtiyaz15@users.noreply.github.com>
2019-09-11 18:38:47 +0530

    Add files via upload

---
+++ TableViewApp/TableViewApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TableViewApp
+//
+//  Created by Felix ITs 08 on 11/09/19.
+//  Copyright © 2019 Felix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+       
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
